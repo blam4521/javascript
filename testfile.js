74d7181e1888f9af743b2782cdbc7ae1b6f8024a
Barry Lam <blam452@gmail.com>
2018-01-19 16:23:16 -0500

    first javascript commit

---
+++ testfile.js
@@ -0,0 +1,12 @@
+// Week Two Javascript
+// console.log("mewo meow moew")
+/*
+var sum =4242342342342342342;
+*/
+// var pi = 3.124;
+
+// compute the result of 
+//  789 / 12345
+var results = 789 / 12345;
+console.log(results);
+
